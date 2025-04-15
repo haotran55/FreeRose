37ff87e06b258e9ea8ed3f71f23ccd4ec544d425
haotran55 <haot54592@gmail.com>
2025-04-15 22:37:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ app = FastAPI()
 
 # Danh sách URL ảnh sẵn có
 images = [
-    "https://i.pinimg.com/736x/65/39/30/6539305fb14952972dabefd10d4402b3.jpg",
-    "https://anhnail.com/wp-content/uploads/2024/10/Hinh-gai-xinh-k8-cute.jpg",
-    "https://1nedrop.com/wp-content/uploads/2024/10/gai-xinh-trung-quoc-57fKByew.jpg"
-    "https://tophinhanh.net/wp-content/uploads/2023/12/Hinh-anh-gai-xinh-k8-1.jpg"
+    "https://files.catbox.moe/it76n1.jpg",
+    "https://files.catbox.moe/v2h0ws.jpg",
+    "https://files.catbox.moe/eeu6mk.jpg",
+    "https://files.catbox.moe/xln7c9.jpg",
+    "https://files.catbox.moe/y5htn1.jpg",
+    "https://files.catbox.moe/qsuf27.jpg",
+    "https://files.catbox.moe/0zj5tk.jpg",
+    "https://files.catbox.moe/yigtfg.jpg",
+    "https://files.catbox.moe/5vwmk6.jpg",
+    "https://files.catbox.moe/w3ygwm.jpg",
+    "https://files.catbox.moe/pvicqh.jpg",
+    "https://files.catbox.moe/6b8u5h.jpg",
+    "https://files.catbox.moe/iixrhj.jpg",
+    "https://files.catbox.moe/6b8u5h.jpg",
+    "https://files.catbox.moe/8biq07.jpg",
+    "https://files.catbox.moe/bve4dc.jpg"
 ]
 
 @app.get("/")
